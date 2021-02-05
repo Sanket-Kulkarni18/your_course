7fa1d741d73b78b06e42610160d15fff7688e8db
Sanket <kulkarnisanket974@gmail.com>
2021-02-05 13:32:41 +0530

    second commmit

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,6 +1,28 @@
+import React, { useState } from "react";
+
 const HomePage = ()=> {
+    const [playlistStr , setPlaylistStr] = useState("");
+    const [url , setUrl] = useState("");
+    const handlesubmit=(e)=>{
+        e.preventDefault();
+        var urlString = url;
+        let startIndex = urlString.search("list=");
+        startIndex+=5;
+        urlString = urlString.substring(startIndex, url.length);
+
+        let endIndex = urlString.search("&");
+        endIndex = (endIndex === -1) ? (url.length) : (endIndex);
+
+        let playlistId = urlString.substring(0,endIndex);
+        console.log(playlistId);
+        const playlistStr=playlistId;
+        // setPlaylistStr("");
+    }
     return (
-        <div>Home Page</div>
+        <div>Home Page 
+            <input type ="text" value={url} onChange={e=>setUrl(e.target.value)}/>
+            <button onClick={handlesubmit}>click</button>
+        </div>
     )
 }
 
